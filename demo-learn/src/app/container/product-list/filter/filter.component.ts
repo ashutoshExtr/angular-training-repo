b16a26db76391dabc595a6a30626ec95e19872fd
Ashutosh Dodamani <adodamani@extremenetworks.com>
2023-12-29 11:19:50 +0530

    created a custom event and. method for it whenever changes occur

--- demo-learn/src/app/container/product-list/filter/filter.component.ts
+++ demo-learn/src/app/container/product-list/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -15,5 +15,17 @@ export class FilterComponent {
   @Input()
   outOfStock: number = 0;
 
+
+  //we want to raise this event whenever there is change in selection of radio button 
+  
+  selectedFilterRadioButtonChanged:EventEmitter<string> = new EventEmitter<string>();
+
+
   selectedFilterRadioButton: string = 'all';
+
+  onSelectedFilterRadioButtonChanged(){
+    //here only child knows what value is selected
+    //console.log(this.selectedFilterRadioButton);
+    this.selectedFilterRadioButtonChanged.emit(this.selectedFilterRadioButton);
+  }
 }
