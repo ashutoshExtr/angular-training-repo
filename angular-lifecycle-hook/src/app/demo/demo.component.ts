2c4c78b637d860e5ce1abd6ab80a871c9a02824f
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-01 17:25:29 +0530

    ng on Init usage

--- angular-lifecycle-hook/src/app/demo/demo.component.ts
+++ angular-lifecycle-hook/src/app/demo/demo.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.css'
 })
-export class DemoComponent implements OnChanges{
+export class DemoComponent implements OnChanges,OnInit{
 
   title: string = 'Demo Component';
   @Input() message: string;
+  @ViewChild('temp') tempPara: ElementRef;
   
   constructor(){
     console.log('Demo component constructor called');
-    console.log(this.title);
-    console.log(this.message);
+    //console.log(this.title);
+    //console.log(this.message);
   }
 
   //this will be called after message property will be initilazed with the value which we are passing from parent component
@@ -21,8 +22,12 @@ export class DemoComponent implements OnChanges{
   //to get the previous and current value of input property - pass the parameter i.e changes:SimpleChanges
   ngOnChanges(changes: SimpleChanges){
     console.log('ngOnChanges called');
-    console.log(this.message);
-    console.log(changes);
-    
+    //console.log(this.message);
+    //console.log(changes);
+  }
+
+  ngOnInit(){
+    console.log('ngOnInit Hook called');
+    console.log(this.tempPara.nativeElement.innerHTML);
   }
 }
