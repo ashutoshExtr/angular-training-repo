4f9843d6861488bc83f1f10e4372d8e502aef3b5
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-02 14:18:06 +0530

    Using Renderer2 Class

--- demo-learn/src/app/CustomDirectives/SetBackground.directive.ts
+++ demo-learn/src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 
 @Directive({
@@ -14,13 +14,21 @@ export class SetBackground implements OnInit{
 
     //private element: ElementRef;
     
-    constructor(private element: ElementRef) {
-        
+    //private renderer: Renderer2;
+    constructor(private element: ElementRef, private renderer: Renderer2) {
+        //this.renderer = renderer;
     }
 
 
     ngOnInit(){
-        this.element.nativeElement.style.backgroundColor = 'grey';
-        this.element.nativeElement.style.color = 'white';
+        //this.element.nativeElement.style.backgroundColor = 'grey';
+        //this.element.nativeElement.style.color = 'white';
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'grey');
+        this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+        //this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is title');
     }
+
+    // nativeElement gives direct access to the DOM. this is not recoomended way to do.
+    // we will make use of RENDERER2 class. 
+    // Renderer2 allows us to manipulate the DOM, without accessing the dom elements directly, by providing a layer of abstrcation betn DOM element and component code.
 }
