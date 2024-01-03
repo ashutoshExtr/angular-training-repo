8739bba5048b822750c684cf69f3700728976ad8
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-03 15:44:38 +0530

    created components and added basic content and styles

---
+++ angular-service-dependency-injection/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HomeComponent } from './header/home/home.component';
+import { HeroComponent } from './header/home/hero/hero.component';
+import { SidebarComponent } from './header/home/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './header/admin/user-detail/user-detail.component';
+import { UserListComponent } from './header/admin/user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    HomeComponent,
+    HeroComponent,
+    SidebarComponent,
+    UserDetailComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
