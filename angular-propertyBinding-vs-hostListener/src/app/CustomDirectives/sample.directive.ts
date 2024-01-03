c5e6742ad72830df75105a76cf8b12f2510918d5
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-03 09:27:37 +0530

    host binding usage

--- angular-propertyBinding-vs-hostListener/src/app/CustomDirectives/sample.directive.ts
+++ angular-propertyBinding-vs-hostListener/src/app/CustomDirectives/sample.directive.ts
@@ -1,11 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appSample]'
 })
 export class SampleDirective {
 
-  inputValue: string = 'Hi There!'
+  //this is how we need to bind the property of dom element in case of DIRECTIVE
+  @HostBinding('value') inputValue: string = 'Hi There!';
 
   constructor() { }
 
