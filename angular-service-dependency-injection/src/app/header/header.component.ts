dc94f37b0493ab9c82a3df0c0986542a36356226
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-03 19:17:04 +0530

    using angular to inject the instance,instead of creating the instance on our own.

--- angular-service-dependency-injection/src/app/header/header.component.ts
+++ angular-service-dependency-injection/src/app/header/header.component.ts
@@ -4,7 +4,10 @@ import { SubscribeService } from '../Services/subscribe.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  providers: [SubscribeService],
+  //what instance we want we neeed to provide here
+  // WHAT TO PROVIDE
 })
 export class HeaderComponent {
   selectedTab: string = 'home';
@@ -19,8 +22,15 @@ export class HeaderComponent {
     this.selectedTab = 'admin';
   }
 
+  // HOW TO PROVIDE
+  constructor(private subService: SubscribeService){
+
+  }
+
   OnSubscribe(){
-    let subService = new SubscribeService();
-    subService.OnSubscribeClicked('monthly');
+    //now instead of creating the instance on our own, we are asking angular to inject the instance.
+    //header dependent on subscribeservice class. leading to tightly coupled
+    //let subService = new SubscribeService();
+    this.subService.OnSubscribeClicked('monthly');
   }
 }
--- angular-service-dependency-injection/src/app/header/home/hero/hero.component.ts
+++ angular-service-dependency-injection/src/app/header/home/hero/hero.component.ts
@@ -4,11 +4,23 @@ import { SubscribeService } from '../../../Services/subscribe.service';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  providers: [SubscribeService],
+  //what instance we want we neeed to provide here
+  // WHAT TO PROVIDE
 })
 export class HeroComponent {
+  //hero coponent  has dependency on subscribeservice class.
+
+   // HOW TO PROVIDE
+  constructor(private subService: SubscribeService){
+
+  }
+
+
+
   OnSubscribe(){
-    let subService = new SubscribeService();
-    subService.OnSubscribeClicked('yearly');
+    //let subService = new SubscribeService();
+    this.subService.OnSubscribeClicked('yearly');
   }
 }
