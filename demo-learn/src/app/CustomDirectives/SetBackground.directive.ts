f685b6a55372af252bedefcdd6e95e65807ed0d2
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-03 10:52:08 +0530

    modified way to do property binding in directive

--- demo-learn/src/app/CustomDirectives/SetBackground.directive.ts
+++ demo-learn/src/app/CustomDirectives/SetBackground.directive.ts
@@ -14,8 +14,11 @@ export class SetBackground implements OnInit{
 
     //private element: ElementRef;
     
-    @Input('setBackground') backColor:string = 'grey';
-    @Input() textColor:string = 'white';
+    //@Input('setBackground') backColor:string = 'grey';
+    //@Input() textColor:string = 'white';
+
+    //in case we want to use both the properties we need to do like this
+    @Input('setBackground') changeTextAndBackColor: { backColor: string, textColor: string };
 
     //private renderer: Renderer2;
     constructor(private element: ElementRef, private renderer: Renderer2) {
@@ -26,8 +29,8 @@ export class SetBackground implements OnInit{
     ngOnInit(){
         //this.element.nativeElement.style.backgroundColor = 'grey';
         //this.element.nativeElement.style.color = 'white';
-        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.backColor);
-        this.renderer.setStyle(this.element.nativeElement, 'color', this.textColor);
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.changeTextAndBackColor.backColor);
+        this.renderer.setStyle(this.element.nativeElement, 'color', this.changeTextAndBackColor.textColor);
         //this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is title');
     }
 
