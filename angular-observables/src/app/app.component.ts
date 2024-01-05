90d026ce5d78984c680ab30eaa7d2e7df6a93d30
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-05 19:20:10 +0530

    complete method in observable

--- angular-observables/src/app/app.component.ts
+++ angular-observables/src/app/app.component.ts
@@ -18,9 +18,10 @@ export class AppComponent {
     setTimeout( () =>  { observer.next(1) }, 1000);
     setTimeout( () =>  { observer.next(2) }, 2000);
     setTimeout( () =>  { observer.next(3) }, 3000);
-    setTimeout( () =>  { observer.error(new Error('error try later')) }, 3000);
+    //setTimeout( () =>  { observer.error(new Error('error try later')) }, 3000);
     setTimeout( () =>  { observer.next(4) }, 4000);
     setTimeout( () =>  { observer.next(5) }, 5000);
+    setTimeout( () =>  { observer.complete() }, 7000);
   })
 
   GetAsyncData(){
@@ -32,6 +33,9 @@ export class AppComponent {
     },
     (err) => {
       alert( err.message)
+    },
+    () => {
+      alert('you are done');
     }
     );
   }
