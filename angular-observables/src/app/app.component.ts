09ceb42efe6d8ec09d863f453d08a4dacb5a75e6
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-05 19:04:29 +0530

    creating observables

--- angular-observables/src/app/app.component.ts
+++ angular-observables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observables';
+
+  data: any[] = [];
+
+  //create observable
+  myObservable = new Observable((observer) => {
+    //to emit values we need to use next method
+    // observer.next([1,2,3,4,5]);
+    setTimeout( () =>  { observer.next(1) }, 9000);
+    observer.next(2);
+    observer.next(3);
+    observer.next(4);
+    observer.next(5);
+  })
+
+  GetAsyncData(){
+    //this code is obeserver
+    this.myObservable.subscribe((val :any) => {
+      // this.data =  val;
+      this.data.push(val);
+      //the callback function here is the HANDLER
+    });
+  }
 }
