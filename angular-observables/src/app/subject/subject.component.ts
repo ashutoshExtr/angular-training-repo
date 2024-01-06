7c4537ee4ffaa681f130e305e56da58cd522a861
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-06 13:56:48 +0530

    Behavior Subject usage

--- angular-observables/src/app/subject/subject.component.ts
+++ angular-observables/src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -15,32 +15,45 @@ export class SubjectComponent implements OnInit{
 
     //const subject = new Subject();
 
+    // in case of bahavior subject we can pass INITIAL VALUE
+    const subject = new BehaviorSubject<number>(100);
+
     // observer doesnot emit same value to all its subscribers
     // obs.subscribe((data) => {
     //   console.log(data);
     // })
 
     //in case of subject both subscribe gets same value
-    // subject.subscribe((data) => {
-    //   console.log(data);
-    // })
+    subject.subscribe((data) => {
+      console.log("Subscriber 1:" +data);
+    })
 
-    // subject.subscribe((data) => {
-    //   console.log(data);
-    // })
+    subject.subscribe((data) => {
+      console.log("Subscriber 2:" +data);
+    })
 
-    // subject.next(Math.random());
+    //emit value
+    subject.next(2020);
+
+    // for this subscribe the value will not be 100, it will have value which was last emitted
+    subject.subscribe((data) => {
+      console.log("Subscriber 3:" +data);
+    })
+
+    subject.next(2024);
+
+
 
 
     //AJAX CALL
-    const subject = new Subject();
-    const data = ajax('https://randomuser.me/api/');
+    // const subject = new Subject();
+    // const data = ajax('https://randomuser.me/api/');
 
-    subject.subscribe((res) => console.log(res));
-    subject.subscribe((res) => console.log(res));
-    subject.subscribe((res) => console.log(res));
+    // subject.subscribe((res) => console.log(res));
+    // subject.subscribe((res) => console.log(res));
+    // subject.subscribe((res) => console.log(res));
 
-    //here subject is consumer value, not providing
-    data.subscribe(subject);
+    // //here subject is consumer value, not providing
+    // data.subscribe(subject);
   }
 }
