a4e7dbe8e3f8798b72ee8663c76b583cc969bb60
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-06 17:38:24 +0530

    created components

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { ContactUsComponent } from './home/contact-us/contact-us.component';
+import { PopularComponent } from './home/popular/popular.component';
+import { ServicesComponent } from './home/services/services.component';
+import { TestimonyComponent } from './home/testimony/testimony.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    CheckoutComponent,
+    ContactComponent,
+    CoursesComponent,
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent,
+    CourseDetailComponent,
+    BannerComponent,
+    ContactUsComponent,
+    PopularComponent,
+    ServicesComponent,
+    TestimonyComponent
   ],
   imports: [
     BrowserModule,
