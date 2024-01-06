c2469ab7a59c9aa0b0ab593d33b2edb0c466129c
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-06 11:34:11 +0530

    Using SUBJECTS to communicate between two sibling  components

---
+++ angular-observables/src/app/Services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService{
+    //CreateTask: EventEmitter<string> = new EventEmitter<string>();
+
+    CreateTask = new Subject<string>();
+
+    OnCreateTask(value: string){
+        //this.CreateTask.emit(value);
+        this.CreateTask.next(value);
+    }
+}
