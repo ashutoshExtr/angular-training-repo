8aaf0c07e545adb491cbcddc1aeeb06c3a4647c6
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-09 16:36:52 +0530

    router navigation events and added loader to course page while navigating

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { Component, inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-routing';
+
+  showLoader: boolean = false;
+
+  router: Router = inject(Router);
+
+  ngOnInit(){
+    this.router.events.subscribe((routerEvent: Event) => {
+      if(routerEvent instanceof NavigationStart){
+        this.showLoader = true;
+      }
+
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError) {
+        this.showLoader = false;
+      }
+      
+    })
+  }
 }
