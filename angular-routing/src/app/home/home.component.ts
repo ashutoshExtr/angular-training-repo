8a9a288ba6d773b78a0a97d1a9482d9b45962e63
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-09 11:26:58 +0530

    fragment in routes

--- angular-routing/src/app/home/home.component.ts
+++ angular-routing/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  //fragment alllows us to jump to the section or content on html page
+  ngOnInit(){
+    this.activeRoute.fragment.subscribe((data) => {
+      console.log(data);
+      this.JumpToSection(data);
+    });
+  }
+
+  JumpToSection(section){
+    document.getElementById(section).scrollIntoView({behavior: 'auto'});
+}
 }
