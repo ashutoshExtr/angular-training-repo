537fd569cc396cddfecfb786b586d0d83b37799e
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-01-09 10:15:42 +0530

    retrive route parameter value using observables

--- angular-routing/src/app/courses/course-detail/course-detail.component.ts
+++ angular-routing/src/app/courses/course-detail/course-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Course } from '../../Models/course';
 import { CourseService } from '../../Services/course.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,21 +8,39 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.css'
 })
-export class CourseDetailComponent implements OnInit {
+export class CourseDetailComponent implements OnInit,OnDestroy {
   selectedCourse: Course;
   courseId: number;
 
   courseService: CourseService = inject(CourseService);
   activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  paramMapObs;
 
   ngOnInit(){
     // this.courseId =  this.activeRoute.snapshot.params['id'];
     //this will return the value stored in ID parameter
 
-    this.courseId =  +this.activeRoute.snapshot.paramMap.get('id');
+
+    //snapshot property returns only the initial value of route parameter. if route parameter changes, snapshot propertyu wont return the updated value. so we will use observable
+    //this.courseId =  +this.activeRoute.snapshot.paramMap.get('id');
     //console.log(this.courseId);
 
-    this.selectedCourse =   this.courseService.courses.find(course => course.id === this.courseId)
+    //everytime route value changes, param observable is going to emit that updated value
+    // this.activeRoute.params.subscribe((data)=> {
+    //   this.courseId = +data['id'];
+    //   this.selectedCourse =   this.courseService.courses.find(course => course.id === this.courseId)
+    // })
+
+    this.paramMapObs =  this.activeRoute.paramMap.subscribe((data)=> {
+      this.courseId = +data.get('id');
+      this.selectedCourse =   this.courseService.courses.find(course => course.id === this.courseId)
+    })
+
+    //this.selectedCourse =   this.courseService.courses.find(course => course.id === this.courseId)
     
+  }
+
+  ngOnDestroy(){
+    this.paramMapObs.unsubscribe();
   }
 }
