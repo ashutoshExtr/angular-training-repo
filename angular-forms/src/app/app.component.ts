09c13448856b08c3513edc11a2a510360b26c695
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-04-27 00:19:25 +0530

    WIP

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -9,6 +9,27 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'angular-forms';
 
+  firstName: string = '';
+  lastName: string = '';
+  dob: string = '';
+  emailAddress: string = '';
+  gender: string = '';
+  country: string = '';
+  city: string = '';
+  region: string = '';
+  postal: string = '';
+  userName: string = '';
+  IsAgreed: boolean = false;
+
+  genders = [
+    {id: 'check-male', value: 'male', display: 'Male'},
+    {id: 'check-female', value: 'female', display: 'Female'},
+    {id: 'check-other', value: 'other', display: 'Prefer not to say'},
+  ]
+
+
+  defaultGender: string = 'male';
+
   @ViewChild('regform') form! : NgForm;
 
   OnFormSubmitted(){
