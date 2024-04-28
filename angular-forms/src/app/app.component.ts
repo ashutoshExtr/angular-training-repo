6256c26b6b0d43720ae29fc955ef0613c0a81073
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-04-28 12:18:45 +0530

    setvalue and pathcvalue for updating the form

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -40,4 +40,63 @@ export class AppComponent {
     console.log(this.form.value.address.country);
     console.log(this.form.value.address.city);
   }
+
+
+  GenerateUsername(){
+    let username = '';
+
+    if(this.firstName.length >= 3){
+      username += this.firstName.slice(0, 3);
+    }
+    else {
+      username += this.firstName;
+    }
+
+    if(this.lastName.length >= 3){
+      username += this.lastName.slice(0, 3);
+    }
+    else {
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.dob);
+    username += datetime.getFullYear();
+
+    username = username.toLowerCase();
+
+    console.log(username);
+
+      // this.form.controls['username'].value = username;
+    // console.log(this.form.value.username)
+
+
+    //update the form using setvalue method
+    // this.form.setValue({
+    //   firstname: this.form.value.firstname,
+    //   lastname: this.form.value.lastname,
+    //   email: this.form.value.email,
+    //   phone: this.form.value.phone,
+    //   dob: this.form.value.dob,
+    //   gender: this.form.value.gender,
+    //   username: username,
+    //   address: {
+    //     street1: this.form.value.address.street1,
+    //     street2: this.form.value.address.street2,
+    //     country: this.form.value.address.country,
+    //     city: this.form.value.address.city,
+    //     region: this.form.value.address.region,
+    //     postal: this.form.value.address.postal
+    //   }
+    // })
+
+
+    this.form.form.patchValue({
+      username: username,
+      address: {
+        country: 'Japan'
+      }
+    })
+
+
+  }
 }
