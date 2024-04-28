00e5d54c468549f71b67b0c8b2ef476a1d734e4a
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-04-28 17:47:49 +0530

    creating custom validator

---
+++ angular-reactive-forms/src/app/Validators/noSpaceAllowed.validator.ts
@@ -0,0 +1,13 @@
+import { AbstractControl, FormControl } from "@angular/forms";
+
+export class CustomValidators{
+    static noSpaceAllowed(control: FormControl){
+        if(control.value != null && control.value.indexOf(' ') != -1){
+            return {noSpaceAllowed: true}
+        }
+        return null;
+    }
+
+    
+}
+
