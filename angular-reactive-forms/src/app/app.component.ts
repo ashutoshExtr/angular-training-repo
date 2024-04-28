157a5f3af5a93245144aa3485fd252b4ddb1f8d2
Ashutosh Dodamani <adodamani@extremenetworks.com>
2024-04-28 13:36:59 +0530

    converting form into reactive form

--- angular-reactive-forms/src/app/app.component.ts
+++ angular-reactive-forms/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'angular-reactive-forms';
+
+  reactiveForm!: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname:new FormControl(null),
+      lastname: new FormControl(null),
+      email: new FormControl(null),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+      street: new FormControl(null, Validators.required),
+      country: new FormControl('India', Validators.required),
+      city: new FormControl(null),
+      region: new FormControl(null),
+      postal: new FormControl(null, Validators.required),
+      
+    })
+  }
+
+  OnFormSubmitted(){
+    console.log(this.reactiveForm);
+    console.log(this.reactiveForm.value);
+  }
 }
